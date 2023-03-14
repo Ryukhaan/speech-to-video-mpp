346a59ac1cc990f374e234243db08f81727c8267
Ryukhaan <remi.decelle@protonmail.com>
2023-03-14 16:13:37 +0100

    Debug

--- third_part/emb/qnt.py
+++ third_part/emb/qnt.py
@@ -65,10 +65,10 @@ def encode(wav: Tensor, sr: int, device="cuda"):
     # Parameters
     n_fft = 800
     hop_size = 200
-    win_size = 1600
+    win_size = 800
     # Padding
-    #p1d = (win_size//2, win_size//2)
-    #wav = torch.nn.functional.pad(wav, p1d)
+    p1d = (win_size//2, win_size//2)
+    wav = torch.nn.functional.pad(wav, p1d)
 
     wav = wav.to(device)
     mel_chunks = []
