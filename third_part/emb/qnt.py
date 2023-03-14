b4032675c62b88619b99d7f94e68c84a5de432c8
Ryukhaan <remi.decelle@protonmail.com>
2023-03-14 14:23:14 +0100

    Debug

--- third_part/emb/qnt.py
+++ third_part/emb/qnt.py
@@ -80,8 +80,8 @@ def encode(wav: Tensor, sr: int, device="cuda"):
         mel_chunks.append(encoded_frames)
         i += 1
     #encoded_frames = model.encode(wav)
-    print([encoded[0].shape for encoded in encoded_frames])
-    qnt = torch.cat([encoded[0] for encoded in encoded_frames], dim=0)  # (b q t)
+    #print([encoded.shape for encoded in mel_chunks])
+    qnt = torch.cat([encoded[0] for encoded in mel_chunks], dim=0)  # (b q t)
     print(qnt.shape)
     return qnt
 
