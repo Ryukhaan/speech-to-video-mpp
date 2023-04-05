cffebccdbf5d4498100d1f3daa0430be1340a0d3
Ryukhaan <remi.decelle@protonmail.com>
2023-04-05 13:23:19 +0200

    Empty cache

--- inference.py
+++ inference.py
@@ -29,6 +29,7 @@ args = options()
 
 def main():    
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch.cuda.empty_cache()
     print('[Info] Using {} for inference.'.format(device))
     os.makedirs(os.path.join('temp', args.tmp_dir), exist_ok=True)
 
@@ -377,6 +378,7 @@ def find_best_audio():
         np.save('temp/' + base_name + '_best_audio.npy', best_vid)
     else:
         args.face = np.load('temp/' + base_name + '_best_audio.npy')
+    print(args.face)
 
 if __name__ == '__main__':
     find_best_audio()
