214baabf24e58a56f9923f59eadc3ab4a3c07d0b
Ryukhaan <remi.decelle@protonmail.com>
2023-04-05 12:33:16 +0200

    Match best vids - corrections

--- inference.py
+++ inference.py
@@ -360,14 +360,13 @@ def find_best_audio():
         src_mel = audio.melspectrogram(src_wav)
         _, src_length = src_mel.shape
         sim = np.inf
+        best_vid = ""
         for file in tqdm(audio_database, desc='[Step 0 bis] Finding best audio:'):
-            print(file, args.audio)
-            continue
             if file == audio_file: continue
             dst_wav = audio.load_wav(file, 16000)
             dst_mel = audio.melspectrogram(dst_wav)
             _, dst_length = dst_mel.shape
-            if dst_length < src_length:
+            if dst_length <= src_length:
                 dst_mel = np.pad(dst_mel, ((0,0),(0,src_length-dst_length)))
                 current_sim = np.mean(np.linalg.norm(src_mel - dst_mel, axis=1))
                 if current_sim < sim:
