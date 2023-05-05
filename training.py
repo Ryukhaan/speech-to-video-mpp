19f12cc78871b567e5a9dcfec9745b8afaa93aa8
Ryukhaan <remi.decelle@protonmail.com>
2023-05-05 15:35:52 +0200

    Training file added. Save imgs step 5.

--- training.py
+++ training.py
@@ -47,8 +47,8 @@ class VGGPerceptualLoss(torch.nn.Module):
         self.blocks = torch.nn.ModuleList(blocks)
         self.transform = torch.nn.functional.interpolate
         self.resize = resize
-        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
-        self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).to('cuda'))
+        self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).to('cuda'))
 
     def forward(self, input, target, feature_layers=[0, 1, 2, 3], style_layers=[]):
         if input.shape[1] != 3:
@@ -60,8 +60,8 @@ class VGGPerceptualLoss(torch.nn.Module):
             input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
             target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
         loss = 0.0
-        x = input
-        y = target
+        x = input.to('cuda')
+        y = target.to('cuda')
         for i, block in enumerate(self.blocks):
             x = block(x)
             y = block(y)
