2ec506f902a1728e2d1bd74f2c315ee18c5d18ee
Ryukhaan <remi.decelle@protonmail.com>
2023-08-31 12:26:49 +0200

    Try 2048p

--- inference.py
+++ inference.py
@@ -212,7 +212,7 @@ def main():
                                in_size=512, channel_multiplier=2, narrow=1,
                                model='GPEN-BFR-512', use_sr=False)
     enhancer = FaceEnhancement(args, base_dir='checkpoints',
-                               in_size=512, channel_multiplier=2, narrow=1,
+                               in_size=1024, channel_multiplier=2, narrow=1,
                                model='GPEN-BFR-2048', use_sr=True)
 
     imgs_enhanced = []
