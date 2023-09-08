5f40c5bead3ba1529175aa3ed8a920cd54814f84
Ryukhaan <remi.decelle@protonmail.com>
2023-09-08 09:51:57 +0200

    Debugging RealESR

--- inference.py
+++ inference.py
@@ -213,7 +213,7 @@ def main():
                                in_size=512, channel_multiplier=2, narrow=1, sr_scale=4,
                                model='GPEN-BFR-512', use_sr=False)
     enhancer = FaceEnhancement(args, base_dir='checkpoints',
-                               in_size=2048, channel_multiplier=2, narrow=1, sr_scale=4,
+                               in_size=2048, channel_multiplier=2, narrow=1, sr_scale=2,
                                sr_model=None,
                                model='GPEN-BFR-2048', use_sr=True)
 
