762a7d329891c82802df10e547ea37dd82e3729c
Ryukhaan <remi.decelle@protonmail.com>
2023-09-14 11:16:52 +0200

    Adding preprocessing package

---
+++ preprocessing/audio2codes.py
@@ -0,0 +1,99 @@
+import os
+import glob
+import gc
+
+from encodec import EncodecModel
+from encodec.utils import convert_audio
+
+import torch
+import torchaudio
+import cv2
+import numpy as np
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", help="Directory to the dataset")
+    parser.add_argument("--outdir", default=None, type=str, help="Directory to save audio")
+    parser.add_argument("--bandwidth", default=24.0, type=float, help="Bandwidth value (by default 24.0)")
+    parser.add_argument("--t", default=5, type=int, help="Number of frames as input")
+    return parser.parse_args()
+
+def read_video(video):
+    video_stream = cv2.VideoCapture(video)
+    fps = video_stream.get(cv2.CAP_PROP_FPS)
+    number_of_frames = 0
+    while True:
+        still_reading, frame = video_stream.read()
+        if not still_reading:
+            video_stream.release()
+            break
+        number_of_frames += 1
+    return number_of_frames, fps
+def padding(audio, t, fps):
+
+def encode_audio(filename, model, t, number_of_frames):
+    wav, sr = torchaudio.load(filename)
+    number_of_frames, fps = read_video(filename[:-3] + 'mp4')
+    idx_multiplier, codes_chunks = int(1. / fps * sr), []
+    p2d = (float(t) / fps, float(t) / fps, 0, 0)
+    wav = torch.nn.functional.pad(wav, p2d, "constant", 0)
+    for i, _ in enumerate(tqdm(range(len(number_of_frames)))):
+        chunk = wav[:, i * idx_multiplier:(i + t) * idx_multiplier]
+        chunk = convert_audio(chunk, sr, model.sample_rate, model.channels)
+        chunk = chunk.unsqueeze(0)
+        # Extract discrete codes from EnCodec
+        with torch.no_grad():
+            encoded_frames = model.encode(chunk)
+        codes = torch.cat([encoded[0] for encoded in encoded_frames], dim=-1)  # [B, n_q, T]
+        codes_chunks.append(codes)
+
+    np.save(base_name + '_codes.npy', np.array(codes_chunks))
+
+if __name__ == "__main__":
+    args = get_args()
+    # Device Cuda or CPU and then set cache empty
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # Load encodec
+    audio_encodec_model = EncodecModel.encodec_model_24khz()
+    audio_encodec_model.set_target_bandwidth(args.bandwidth)
+
+    # Check if dataset is a file or a directory
+    if os.path.isfile(args.dataset):
+        # Load audio
+        wav, sr = torchaudio.load(args.dataset)
+        video_stream = cv2.VideoCapture(args.dataset + name[:-3] + 'mp4')
+        fps = video_stream.get(cv2.CAP_PROP_FPS)
+        # Extract the Audio
+        audio = video.audio
+        # Export the Audio
+        audio.write_audiofile(args.dataset[:-3] + "wav")
+    else:
+        files = glob.glob(args.dataset + "*.mp4", recursive=True)
+        pbar = tqdm(files)
+        for audio in pbar:
+            pbar.set_description("Processing %s" % file.split('/')[-1])
+            name = audio.split('/')[-1]
+            # Load audio
+
+            # Load video
+            t = 5
+
+            # Extract the Audio
+            audio = video.audio
+            # Export the Audio
+            if args.outdir:
+                audio.write_audiofile(args.outdir + file.split('/')[-1][:-3] + "wav", verbose=False, logger=None)
+            else:
+                audio.write_audiofile(file[:-3] + "wav", verbose=False, logger=None)
+
+
+    print(sr)
+
+    # for i, _ in enumerate(tqdm(range(0, wav.shape[1], idx_multiplier), total=int(wav.shape[1] / idx_multiplier))):
+
+    print(mel_chunks[0], mel_chunks[4 * t], mel_chunks[-1].shape)
+    print("[Step 4 bis] Load audio; Length of mel chunks: {}".format(len(mel_chunks)))
+    exit()
--- preprocessing/video2audio.py
+++ preprocessing/video2audio.py
@@ -10,27 +10,25 @@ def get_args():
     parser.add_argument("--outdir", default=None, type=str, help="Directory to save audio")
     return parser.parse_args()
 
+def read_and_write(filename):
+    # Load the Video
+    video = moviepy.editor.VideoFileClip(filename)
+    # Extract the Audio
+    audio = video.audio
+    # Export the Audio (change mp4 to wav)
+    audio.write_audiofile(filename[:-3] + "wav", verbose=False, logger=None)
+
 if __name__ == "__main__":
     args = get_args()
     # Check if dataset is a file or a directory
     if os.path.isfile(args.dataset):
-        # Load the Video
-        video = moviepy.editor.VideoFileClip(args.dataset)
-        # Extract the Audio
-        audio = video.audio
-        # Export the Audio
-        audio.write_audiofile(args.dataset[:-3] + "wav")
+        read_and_write(args.dataset)
     else:
         files = glob.glob(args.dataset + "*.mp4", recursive=True)
         pbar = tqdm(files)
         for file in pbar:
             pbar.set_description("Processing %s" % file.split('/')[-1])
-            # Load the Video
-            video = moviepy.editor.VideoFileClip(file)
-            # Extract the Audio
-            audio = video.audio
-            # Export the Audio
             if args.outdir:
-                audio.write_audiofile(args.outdir + file.split('/')[-1][:-3] + "wav", verbose=False, logger=None)
+                read_and_write(args.outdir + file.split('/')[-1])
             else:
-                audio.write_audiofile(file[:-3] + "wav", verbose=False, logger=None)
+                read_and_write(file)
