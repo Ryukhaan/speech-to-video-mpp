dd54130050a6fc83c4e140fcb65297759719c6e0
Ryukhaan <remi.decelle@protonmail.com>
2023-09-14 11:27:15 +0200

    Adding preprocessing package

--- training.py
+++ training.py
@@ -55,8 +55,8 @@ class ArcFaceLoss(torch.nn.Module):
         torch.cuda.empty_cache()
         net_recon = load_face3d_net(self.face3d_net_path, self.device)
         lm3d_std = load_lm3d(self.lm3d)
-
-        W, H = y_pred.shape
+        print(y_pred.shape)
+        _, W, H = y_pred.shape
         lm_idx = lm[idx].reshape([-1, 2])
         if np.mean(lm_idx) == -1:
             lm_idx = (lm3d_std[:, :2]+1) / 2.
