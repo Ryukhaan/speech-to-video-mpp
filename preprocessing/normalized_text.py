d1c814c4ec352dcca737964067493fa7c481b6b8
Ryukhaan <remi.decelle@protonmail.com>
2023-09-20 14:17:56 +0200

    Adding normalized text function

--- preprocessing/normalized_text.py
+++ preprocessing/normalized_text.py
@@ -10,19 +10,21 @@ def get_args():
     return parser.parse_args()
 
 def remove_header(text):
-    return ''.join(text.split(':')[:-1])
+    return ''.join(text.split(':')[1:])
 
 def remove_footer(text):
     return ''.join(text.splitlines()[0])
 
 if __name__ == "__main__":
     args = get_args()
+    assert args.outdir[-1] == '/', print('Output directory should have / at the end')
     # Check if dataset is a file or a directory
     if os.path.isfile(args.dataset):
+        text = ''
         with open(args.dataset, 'r') as file:
-            text = file.read()
-            text = remove_footer(remove_header(text))
-            print(text)
+            text = remove_footer(remove_header(file.read()))
+        with open(args.dataset, 'w') as file:
+            file.write(text)
     else:
         files = glob.glob(args.dataset + "**/*.mp4", recursive=True)
         pbar = tqdm(files)
@@ -31,12 +33,11 @@ if __name__ == "__main__":
             pbar.set_description("Processing %s" % file.split('/')[-1])
             if args.outdir:
                 with open(file, 'r') as f:
-                    text = f.read()
-                    text = remove_footer(remove_header(text))
-                    print(text)
-                #read_and_write(args.outdir + file.split('/')[-1])
+                    text = remove_footer(remove_header(f.read()))
+                with open(args.outdir + file.split('/')[-1], 'w') as f:
+                    f.write(text)
             else:
                 with open(file, 'r') as f:
-                    text = f.read()
-                    text = remove_footer(remove_header(text))
-                    print(text)
+                    text = remove_footer(remove_header(f.read()))
+                with open(file, 'w') as f:
+                    f.write(text)
