1dba591696d8d5d9c2a5d74a9492235ce3d73cc2
Ryukhaan <remi.decelle@protonmail.com>
2023-09-26 14:06:26 +0200

    Debugging img input

--- inference.py
+++ inference.py
@@ -228,7 +228,7 @@ def main():
     enhancer = FaceEnhancement(args, base_dir='checkpoints',
                                in_size=2048, channel_multiplier=2, narrow=1, sr_scale=2,
                                sr_model=None,
-                               model='GPEN-BFR-2048', use_sr=True)
+                               model='GPEN-BFR-2048', use_sr=False)
 
     imgs_enhanced = []
     for idx in tqdm(range(len(imgs)), desc='[Step 5] Reference Enhancement'):
