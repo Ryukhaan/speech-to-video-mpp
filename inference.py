42aed79204fe5a5d24994f80bec61b64dbb53e20
Ryukhaan <remi.decelle@protonmail.com>
2023-10-25 14:29:06 +0200

    Save landmarks as png

--- inference.py
+++ inference.py
@@ -185,9 +185,9 @@ def main():
                 dst_pts = lm[idx][1:16]
                 # TODO
                 # Add resize points coordinate
-                for idx, (x, y) in enumerate(dst_pts):
+                for j, (x, y) in enumerate(dst_pts):
                     xi, yi = int(inverse_scale*x+x1), int(inverse_scale*y+y1)
-                    xj, yj = int(inverse_scale*dst_pts[idx - 1][0]+x1), int(inverse_scale*dst_pts[idx - 1][1]+y1)
+                    xj, yj = int(inverse_scale*dst_pts[j - 1][0]+x1), int(inverse_scale*dst_pts[j - 1][1]+y1)
                     cv2.line(mask, (xj, yj), (xi, yi), 255, 3)
                 #cv2.floodFill(mask, None, (0, 0), 255);
                 #mask = np.bitwise_not(mask)
