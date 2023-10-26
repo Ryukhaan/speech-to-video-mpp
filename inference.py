2852345d08d02d2ad38452b1e355a52e60dd3212
Ryukhaan <remi.decelle@protonmail.com>
2023-10-26 12:14:19 +0200

    Debug facial

--- inference.py
+++ inference.py
@@ -186,7 +186,7 @@ def main():
                 dst_pts = lm[idx][-19:-1]
                 for j, (x,y) in enumerate(lm[idx]):
                     cv2.circle(mask, (x,y), 3, 255, 1)
-                    cv2.putText(mask, str(j), (x+5,y)cv2.FONT_HERSHEY_DUPLEX, 1.5, 255, 2, cv2.LINE_AA)
+                    cv2.putText(mask, str(j), (x+5,y), cv2.FONT_HERSHEY_DUPLEX, 1.5, 255, 2, cv2.LINE_AA)
                 # TODO
                 # Add resize points coordinate
                 #cv2.rectangle(mask, (ox1, oy1), (ox2, oy2), (255, 0, 0), 3)
