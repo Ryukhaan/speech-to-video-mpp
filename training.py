2a268bb84cc85df6637f7e7ad29d857069e86f35
Ryukhaan <remi.decelle@protonmail.com>
2023-12-04 12:01:02 +0100

    Train split

--- training.py
+++ training.py
@@ -765,7 +765,7 @@ if __name__ == "__main__":
 
     filenames = get_image_list(args.data_root, 'train')
     seed = 42
-    train_list, val_list = train_test_split(filenames, random_state=seed, train_size=0.7)
+    train_list, val_list = train_test_split(filenames, random_state=seed, train_size=0.7, test_size=0.3)
     print(len(train_list), len(val_list))
     # Dataset and Dataloader setup
     train_dataset = Dataset(train_list)
