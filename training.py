e40bb9cfe294f3c7b516bf0e833568b19958cf29
Ryukhaan <remi.decelle@protonmail.com>
2023-12-04 14:00:08 +0100

    Modify batch size

--- training.py
+++ training.py
@@ -769,7 +769,7 @@ if __name__ == "__main__":
 
     filenames = get_image_list(args.data_root, 'train')
     seed = 42
-    train_list, val_list = train_test_split(np.array(filenames), random_state=seed, train_size=0.7, test_size=0.3)
+    train_list, val_list = train_test_split(np.array(filenames), random_state=seed, train_size=0.8, test_size=0.2)
     print(len(filenames), len(train_list), len(val_list))
     # Dataset and Dataloader setup
     train_dataset = Dataset(train_list)
