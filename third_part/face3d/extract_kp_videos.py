128a7a77238c8f191c1f169e93b8d4635ee5a20a
Ryukhaan <remi.decelle@protonmail.com>
2023-12-05 11:46:54 +0100

    Debug phones again

--- third_part/face3d/extract_kp_videos.py
+++ third_part/face3d/extract_kp_videos.py
@@ -22,7 +22,7 @@ class KeypointExtractor():
         if isinstance(images, list):
             keypoints = []
             if info:
-                i_range = tqdm(images)
+                i_range = images
             else:
                 i_range = images
 
