06635adb38e7caa21e7799c67960b3dbf1c81053
Ryukhaan <remi.decelle@protonmail.com>
2023-12-05 11:43:19 +0100

    Debug phones again

--- training.py
+++ training.py
@@ -162,7 +162,8 @@ class Dataset(object):
                 phone = "v"
             self.phones_per_ms[int(1000 * start):int(1000 * end)] = self.dictionary.index(phone)
         self.phones_per_ms = np.pad(self.phones_per_ms, (100, 100), 'constant', constant_values=0)
-        phones = self.phones_per_ms[100 + (start_frame-2)*200 : 100 + (start_frame-2+lnet_T)*200 ]
+        phones = self.phones_per_ms[:, 100 + (start_frame-2)*200 : 100 + (start_frame-2+lnet_T)*200 ]
+        print(phones.shape)
         return phones
 
     def prepare_window(self, window):
