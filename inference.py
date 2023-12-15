d39f3387e85a95040266644c5449117f22e9fdfc
Ryukhaan <remi.decelle@protonmail.com>
2023-12-15 17:40:04 +0100

    Debug again orientation mask

--- inference.py
+++ inference.py
@@ -72,7 +72,7 @@ def main():
         mel_chunks.append(mel[:, start_idx : start_idx + mel_step_size])
         i += 1
 
-    mel_chunks = mel_chunks[:24] # Change here length of inference video
+    #mel_chunks = mel_chunks[:24] # Change here length of inference video
     print("[Step 4] Load audio; Length of mel chunks: {}".format(len(mel_chunks)))
     imgs = imgs[:len(mel_chunks)]
     full_frames = full_frames[:len(mel_chunks)]  
