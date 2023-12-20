72f8077e27e8ff63427f3c258773a37bba6aff67
Ryukhaan <remi.decelle@protonmail.com>
2023-12-20 13:12:46 +0100

    Debug training

--- training.py
+++ training.py
@@ -206,6 +206,7 @@ class Dataset(object):
         else:
             #print('[Step 1] Using saved landmarks.')
             self.lm = np.loadtxt( self.all_videos[self.idx].split('.')[0] +'_landmarks.txt').astype(np.float32)
+            print(self.lm.shape)
             self.lm = self.lm[start_frame:start_frame+lnet_T]
             print(self.lm.shape)
             self.lm = self.lm.reshape([len(self.frames_pil), -1, 2])
