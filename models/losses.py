9d9e445e409bf1a78d77cfcc850d6aad8abadfa9
Ryukhaan <remi.decelle@protonmail.com>
2024-01-09 11:50:42 +0100

    Debug getitem

--- models/losses.py
+++ models/losses.py
@@ -133,8 +133,8 @@ class LNetLoss(torch.nn.Module):
 
 
         resizer = torchvision.transforms.Resize((384, 384))
-        y_pred = torch.zeros((face_pred.shape[0], face_true.shape[1], face_pred.shape[2], 384,384))
-        y_true = torch.zeros((face_pred.shape[0], face_true.shape[1], face_pred.shape[2], 384,384))
+        y_pred = torch.zeros((face_pred.shape[0], face_pred.shape[1], face_pred.shape[2], 384,384))
+        y_true = torch.zeros((face_true.shape[0], face_true.shape[1], face_true.shape[2], 384,384))
         for i in range(T):
         #    print( resizer(face_pred[:,i, : ,:, :]).shape)
             y_pred[:,i,:,:,:] = resizer(face_pred[:,i, : ,:, :])
