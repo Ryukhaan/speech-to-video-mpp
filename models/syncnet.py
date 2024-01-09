26b9157c5046d635f0172c119d596ecd74fc58c7
Ryukhaan <remi.decelle@protonmail.com>
2024-01-09 13:42:20 +0100

    Debug LipSync Loss

--- models/syncnet.py
+++ models/syncnet.py
@@ -93,7 +93,7 @@ class SyncNet_color(nn.Module):
             Conv2d(512, 512, kernel_size=1, stride=1, padding=0),)
 
     def forward(self, audio_sequences, face_sequences): # audio_sequences := (B, dim, T)
-        face_embedding = self.face_encoder(torch.sum(face_sequences, dim=1)/3.)
+        face_embedding = self.face_encoder(face_sequences.view(-1, 15, 384, 384))
         audio_embedding = self.audio_encoder(audio_sequences)
 
         audio_embedding = audio_embedding.view(audio_embedding.size(0), -1)
