f9206f88897c361ed2c9161fcac33b848682a364
Ryukhaan <remi.decelle@protonmail.com>
2024-01-09 10:24:58 +0100

    Debug getitem

--- models/losses.py
+++ models/losses.py
@@ -135,8 +135,8 @@ class LNetLoss(torch.nn.Module):
         y_pred = face_pred
         y_true = face_true
         for i in range(T):
-            y_pred[:,:,i,:,:] = torchvision.transforms.Resize((384,384), y_pred[:,:,i,:,:])
-            y_true[:, :, i, :, :] = torchvision.transforms.Resize((384, 384), y_true[:, :, i, :, :])
+            y_pred[:,:,i,:,:] = torchvision.transforms.Resize((384,384), y_pred[:,:,i,:,:].unsqueeze(2))
+            y_true[:, :, i, :, :] = torchvision.transforms.Resize((384, 384), y_true[:, :, i, :, :].unsqueeze(2))
         L1 = torch.nn.L1Loss()
         L_perceptual = VGGPerceptualLoss()
         l1_ = []
