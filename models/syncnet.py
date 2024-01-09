f63ba85db09a9153184843b6e9303d25385927e7
Ryukhaan <remi.decelle@protonmail.com>
2024-01-09 14:24:23 +0100

    Debug LipSync Loss - audio conv

--- models/syncnet.py
+++ models/syncnet.py
@@ -95,7 +95,7 @@ class SyncNet_color(nn.Module):
     def forward(self, audio_sequences, face_sequences): # audio_sequences := (B, dim, T)
         print(audio_sequences.shape)
         face_embedding = self.face_encoder(face_sequences.view(-1, 15, 384, 384))
-        audio_embedding = self.audio_encoder(audio_sequences)
+        audio_embedding = self.audio_encoder(audio_sequences.view(-1,80,5,1))
 
         audio_embedding = audio_embedding.view(audio_embedding.size(0), -1)
         face_embedding = face_embedding.view(face_embedding.size(0), -1)
