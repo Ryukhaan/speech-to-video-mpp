24af03fbb6be4ce525828f38a4eff1dc93583752
Ryukhaan <remi.decelle@protonmail.com>
2024-01-17 16:39:30 +0100

    New mask in enhancement - why no video ?

--- inference.py
+++ inference.py
@@ -227,7 +227,7 @@ def main():
             pp = np.uint8(cv2.resize(np.clip(img, 0 ,255), (width, height)))
             #print(pp.shape, xf.shape, c)
             if args.cropped_image:
-                mm = [0,   0,   0,   0,   0,   0,   0,   0,   0,  0, 255, 255, 255, 0, 0, 0, 0, 0, 0]
+                mm = [255,   0,   0,   0,   0,   0,   0,   0,   0,  0, 255, 255, 255, 0, 0, 0, 0, 0, 255]
                 pp, orig_faces, enhanced_faces = enhancer.process_withmask(pp, xf, mask=mask,
                                                                            face_enhance=True,
                                                                            possion_blending=True,
