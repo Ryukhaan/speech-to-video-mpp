34cddf2eb866258bdd847208a9f25b24d427ef39
Ryukhaan <remi.decelle@protonmail.com>
2024-02-13 13:50:35 +0100

    Lora branch

--- models/LNet.py
+++ models/LNet.py
@@ -129,6 +129,7 @@ class LNet(nn.Module):
         if input_dim_size > 4:
             audio_sequences = torch.cat([audio_sequences[:, i] for i in range(audio_sequences.size(1))], dim=0)
             face_sequences = torch.cat([face_sequences[:, :, i] for i in range(face_sequences.size(2))], dim=0)
+        print(audio_sequences.shape)
         cropped, ref = torch.split(face_sequences, 3, dim=1)
         #if len(cropped.size()) > 4:
         #    cropped = torch.cat([cropped[:,:,i] for i in range(cropped.size(2))], dim=0)
