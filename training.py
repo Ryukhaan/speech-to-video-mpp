a43dc5305f805adbcc92e554e89fb650d4378590
Ryukhaan <remi.decelle@protonmail.com>
2024-02-13 14:33:31 +0100

    Lora branch

--- training.py
+++ training.py
@@ -198,7 +198,7 @@ class Dataset(object):
         return mels
 
     def crop_face(self, frames):
-        print(self.lm.shape, frames.shape)
+        print(self.lm.shape, len(frames))
         frames_pil = [(lm, frame) for frame, lm in zip(frames, self.lm)]
         crops, orig_images, quads = crop_faces(256, frames_pil, scale=1.0, use_fa=True)
         return crops
