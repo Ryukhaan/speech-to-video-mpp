e5e561fae22456ed9a0c19bddb70a0875abd940e
Ryukhaan <remi.decelle@protonmail.com>
2024-02-13 14:34:55 +0100

    Lora branch

--- training.py
+++ training.py
@@ -240,7 +240,7 @@ class Dataset(object):
         else:
             #print('[Step 1] Using saved landmarks.')
             self.lm = np.loadtxt( self.all_videos[self.idx].split('.')[0] +'_landmarks.txt').astype(np.float32)
-            print(lm.shape)
+            print(self.lm.shape)
             self.lm = self.lm[start_frame:start_frame+2*lnet_T]
             self.lm = self.lm.reshape([lnet_T, -1, 2])
 
