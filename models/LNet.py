63feec7c62b0f02377655f8d5e323a632c4d16fc
Ryukhaan <remi.decelle@protonmail.com>
2024-02-13 13:48:39 +0100

    Lora branch

--- models/LNet.py
+++ models/LNet.py
@@ -124,17 +124,16 @@ class LNet(nn.Module):
             )
 
     def forward(self, audio_sequences, face_sequences):
-        print(face_sequences.shape)
         B = audio_sequences.size(0)
         input_dim_size = len(face_sequences.size())
-        #if input_dim_size > 4:
-        #    audio_sequences = torch.cat([audio_sequences[:, i] for i in range(audio_sequences.size(1))], dim=0)
-        #    face_sequences = torch.cat([face_sequences[:, :, i] for i in range(face_sequences.size(2))], dim=0)
+        if input_dim_size > 4:
+            audio_sequences = torch.cat([audio_sequences[:, i] for i in range(audio_sequences.size(1))], dim=0)
+            face_sequences = torch.cat([face_sequences[:, :, i] for i in range(face_sequences.size(2))], dim=0)
         cropped, ref = torch.split(face_sequences, 3, dim=1)
-        if len(cropped.size()) > 4:
-            cropped = torch.cat([cropped[:,:,i] for i in range(cropped.size(2))], dim=0)
-            ref = torch.cat([ref[:,:,i] for i in range(ref.size(2))], dim=0)
-        print(cropped.shape, ref.shape)
+        #if len(cropped.size()) > 4:
+        #    cropped = torch.cat([cropped[:,:,i] for i in range(cropped.size(2))], dim=0)
+        #    ref = torch.cat([ref[:,:,i] for i in range(ref.size(2))], dim=0)
+
         vis_feat = self.encoder(cropped, ref)
         audio_feat = self.audio_encoder(audio_sequences)
         _outputs = self.decoder(vis_feat, audio_feat)
