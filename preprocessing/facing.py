4c07b1036f241923119871cc2ec015eea814a7a7
Ryukhaan <remi.decelle@protonmail.com>
2024-02-20 13:49:42 +0100

    Modify inference files for lora

--- preprocessing/facing.py
+++ preprocessing/facing.py
@@ -169,6 +169,7 @@ class Preprocessor():
         del net_recon
         # load DNet, model(LNet and ENet)
         torch.cuda.empty_cache()
+        print(self.args)
         if self.args.use_lora:
             self.D_Net, self.model = load_lora_model(self.args, device)
         else:
