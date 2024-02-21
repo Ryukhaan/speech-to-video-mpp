a1038003d1a9025bcfd135e21d4e172dd05b25f2
Ryukhaan <remi.decelle@protonmail.com>
2024-02-21 14:37:44 +0100

    Retrain

--- models/losses.py
+++ models/losses.py
@@ -136,16 +136,16 @@ class LoraLoss(torch.nn.Module):
 
         B, C, T, Hin, Win = face_pred.shape
         _, _, _, H, W =  face_true.shape
-        resizer = torchvision.transforms.Resize((H, W))
+        resizer = torchvision.transforms.Resize((Hin, Win))
 
-        y_pred = face_pred.view(B*T, C, Hin, Win)
-        y_true = face_true.view(B*T, C, H, W)
+        y_pred = torch.cat([face_pred[:, :, i] for i in range(face_pred.size(2))], dim=0)
+        y_true = torch.cat([face_pred[:, :, i] for i in range(face_pred.size(2))], dim=0)
 
-        y_pred = resizer(y_pred)
+        #y_pred = resizer(y_pred)
         y_true  = resizer(y_true)
 
         l1_val = self.L1(y_pred, y_true).to(self.device)
-        lp_val = self.L_perceptual(y_pred, y_true).to(self.device)
+        #lp_val = self.L_perceptual(y_pred, y_true).to(self.device)
         lsync_val = self.lip_sync_loss(audio_seq, face_pred).to(self.device)
 
-        return self.lambda_1 * l1_val + self.lambda_p * lp_val + self.lambda_sync * lsync_val
+        return self.lambda_1 * l1_val + self.lambda_sync * lsync_val #+ self.lambda_p * lp_val
