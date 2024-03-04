90a44b61a493dab8fda1bc3884e00f9fa5ec1998
Ryukhaan <remi.decelle@protonmail.com>
2024-03-04 10:48:22 +0100

    Debug Training Loop (again)

--- training.py
+++ training.py
@@ -178,7 +178,7 @@ class Dataset(object):
         print(tmin, tmax)
         text_array = []
         for (ts, te, word) in self.words:
-            if ts >= tmin and te < tmax:
+            if ts < tmax and te >= tmin:
                 text_array.append(word)
         text_tokens = clip.tokenize(text_array).to(self.device)
         text_features = self.clip_model.encode_text(text_tokens)
