d90576b0f17c1020fdde40b2e843cf05a8deac70
Ryukhaan <remi.decelle@protonmail.com>
2024-03-07 15:45:51 +0100

    Big changement in training loop

--- models/losses.py
+++ models/losses.py
@@ -30,7 +30,8 @@ class LipSyncLoss(torch.nn.Module):
 
     def forward(self, audio, y_pred):
         y_pred = y_pred[:, :, :, y_pred.size(3) // 2:]
-        y_pred = torch.cat([y_pred[:, :, i] for i in range(self.number_of_frames)], dim=1)
+        if y.pred.size(0) > 1:
+            y_pred = torch.cat([y_pred[:, :, i] for i in range(self.number_of_frames)], dim=1)
         #audio = audio
         #y_pred = y_pred
         audio_emb, video_emb = self.net(audio, y_pred)
