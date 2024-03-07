dec72d4802981c7a2193a5ec4bf6522ab6d240d9
Ryukhaan <remi.decelle@protonmail.com>
2024-03-07 11:38:22 +0100

    Big changement in training loop

--- futils/inference_utils.py
+++ futils/inference_utils.py
@@ -40,7 +40,7 @@ def train_options():
     parser.add_argument('--pads', nargs='+', type=int, default=[0, 20, 0, 0],
                         help='Padding (top, bottom, left, right). Please adjust to include chin at least')
     parser.add_argument('--face_det_batch_size', type=int, help='Batch size for face detection', default=4)
-    parser.add_argument('--LNet_batch_size', type=int, help='Batch size for LNet', default=16)
+    parser.add_argument('--LNet_batch_size', type=int, help='Batch size for LNet', default=2)
     parser.add_argument('--img_size', type=int, default=384)
     parser.add_argument('--crop', nargs='+', type=int, default=[0, -1, 0, -1],
                         help='Crop video to a smaller region (top, bottom, left, right). Applied after resize_factor and rotate arg. '
