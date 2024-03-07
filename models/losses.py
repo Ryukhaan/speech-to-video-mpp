438ac8967f6854ffe9d93b557160e61c5802a8d2
Ryukhaan <remi.decelle@protonmail.com>
2024-03-07 15:40:22 +0100

    Big changement in training loop

--- models/losses.py
+++ models/losses.py
@@ -147,8 +147,7 @@ class LoraLoss(torch.nn.Module):
             y_true = face_true
         y_pred_up = resizer_up(y_pred)
         y_true_96 = resizer_96(y_true)
-        audio_seq = torch.transpose(audio_seq, 1, 3).squeeze(3).unsqueeze(1)
-        print(audio_seq.shape)
+        audio_seq = torch.transpose(audio_seq[:,:-1], 1, 3)
         l1_val = self.L1(y_pred_up, y_true).to(self.device)
         lp_val = self.L_perceptual(y_pred, y_true_96).to(self.device)
         lsync_val = self.lip_sync_loss(audio_seq, face_pred).to(self.device)
