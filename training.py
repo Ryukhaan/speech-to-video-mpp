6bc814d4ca90ed1fcf60a601b6154bc27a5f8715
Ryukhaan <remi.decelle@protonmail.com>
2024-03-11 16:58:45 +0100

    Integrate LORA again !

--- training.py
+++ training.py
@@ -409,8 +409,8 @@ class Dataset(object):
         indiv_mels = torch.FloatTensor(np.transpose(indiv_mels, (0,3,1,2)))
         #if stabilized_window.size(1) != lnet_T:
         #    return None, None, None, None
-        assert stabilized_window.size == torch.Size([6, 5, 96, 96])
-        assert img_original.size == torch.Size([3, 5, 96, 96])
+        assert stabilized_window.size == torch.Size([6, 5, 96, 96]), print(stabilized_window.shape)
+        assert img_original.size == torch.Size([3, 5, 96, 96]), print(img_original.shape)
         return stabilized_window, indiv_mels, mels, img_original
 
     def save_preprocess(self):
