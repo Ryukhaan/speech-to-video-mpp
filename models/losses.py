db8f6c1c124009452d4a936c7d8db53b0a1fb3b3
Ryukhaan <remi.decelle@protonmail.com>
2024-03-12 10:33:09 +0100

    Adding Total Variation Loss

--- models/losses.py
+++ models/losses.py
@@ -135,7 +135,7 @@ class LoraLoss(torch.nn.Module):
 
         l1_val = self.L1(y_pred_up, y_true).to(self.device)
         lp_val = self.L_perceptual(y_pred_96, y_true_96).to(self.device)
-        lsync_val = self.lip_sync_loss(audio_seq, y_pred_96).to(self.device)
+        lsync_val = self.lip_sync_loss(audio_seq, face_seq).to(self.device)
         tv_val = self.tv_loss(y_pred_up).to(self.device)
         return self.lambda_1 * l1_val \
             + self.lambda_sync * lsync_val \
