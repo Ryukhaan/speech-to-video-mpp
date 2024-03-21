3c0f631bd95120d12bc7575061547372a049259b
Ryukhaan <remi.decelle@protonmail.com>
2024-03-21 15:56:41 +0100

    Adding position in tqdm bar

--- training.py
+++ training.py
@@ -496,12 +496,17 @@ def train(device, model, train_data_loader, test_data_loader, optimizer,
 
             # KeyPoint Extractor Loss
             pred_lms = np.zeros((pred.size(0), lnet_T, 68, 2))
+            true_lms = np.zeros((pred.size(0), lnet_T, 68, 2))
             for b in range(pred.size(0)):
                 fr_pil = [Image.fromarray((255 * pred[b,:,frame].squeeze().detach().cpu().numpy()).astype(np.uint8).reshape(96,96,3)) for frame in range(lnet_T)]
                 pred_lms[b] = kp_extractor.extract_keypoint(fr_pil, 'temp/pred_x12_landmarks.txt')
+                fr_pil = [Image.fromarray(
+                    (255 * y[b, :, frame].squeeze().detach().cpu().numpy()).astype(np.uint8).reshape(96, 96, 3)) for
+                    frame in range(lnet_T)]
+                true_lms[b] = kp_extractor.extract_keypoint(fr_pil, 'temp/pred_x12_landmarks.txt')
             pred_lms = torch.FloatTensor(pred_lms).to(device)
-            lms = lms.to(device)
-            loss_lm = nn.MSELoss()(lms[:,:,48:], pred_lms[:,:,48:])
+            true_lms = torch.FloatTensor(true_lms).to(device)
+            loss_lm = nn.MSELoss()(true_lms[:,:,48:], pred_lms[:,:,48:])
 
             # Extract validity predictions from discriminator
             pred_real = disc_model(pred).detach()
