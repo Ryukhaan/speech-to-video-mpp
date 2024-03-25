cf18b3ec4bf86bfc7c9c04ab15be7d61dc54baf5
Ryukhaan <remi.decelle@protonmail.com>
2024-03-25 13:51:03 +0100

    Change Enhancer

--- inference.py
+++ inference.py
@@ -251,7 +251,7 @@ def main():
     enhancer = FaceEnhancement(args, base_dir='checkpoints',
                                in_size=512, channel_multiplier=2, narrow=1, sr_scale=2,
                                sr_model=None,
-                               model='GPEN-BFR-512', use_sr=True)
+                               model='GPEN-BFR-512', use_sr=False)
 
     imgs_enhanced = []
     for idx in tqdm(range(len(imgs)), desc='[Step 5] Reference Enhancement'):
