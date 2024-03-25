efb115bdbde52fa36a18b6685aa6a35568e822a4
Ryukhaan <remi.decelle@protonmail.com>
2024-03-25 17:10:36 +0100

    Change mm

--- inference.py
+++ inference.py
@@ -374,7 +374,7 @@ def main():
             pp = cv2.resize(pp,  (0,0), fx=0.5, fy=0.5)
 
             mm = [255] * 19
-            pp, orig_faces, enhanced_faces = enhancer.process(pp, xf, bbox=c, face_enhance=True, possion_blending=True, mm=mm)
+            pp, orig_faces, enhanced_faces = enhancer.process(pp, xf, bbox=c, face_enhance=False, possion_blending=False, mm=mm)
             cv2.imwrite('./temp/images/frame{}.png'.format(ip), pp)
             ip+=1
             out.write(pp)
