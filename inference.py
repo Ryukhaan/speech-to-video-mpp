eb664c207cb4d604f47525d1ce4ed2108dd908c0
Ryukhaan <remi.decelle@protonmail.com>
2024-03-25 14:53:32 +0100

    Not using lora load not lora

--- inference.py
+++ inference.py
@@ -249,9 +249,9 @@ def main():
                                model='GPEN-BFR-512', use_sr=False)
 
     enhancer = FaceEnhancement(args, base_dir='checkpoints',
-                               in_size=1024, channel_multiplier=2, narrow=1, sr_scale=2,
+                               in_size=512, channel_multiplier=2, narrow=1, sr_scale=2,
                                sr_model=None,
-                               model='GPEN-BFR-1024', use_sr=False)
+                               model='GPEN-BFR-512', use_sr=True)
 
     imgs_enhanced = []
     for idx in tqdm(range(len(imgs)), desc='[Step 5] Reference Enhancement'):
@@ -368,7 +368,8 @@ def main():
             restored_img, ff, full_mask = [cv2.resize(x, (512, 512)) for x in (restored_img, ff, np.float32(mouse_mask))]
             img = Laplacian_Pyramid_Blending_with_mask(restored_img, ff, full_mask[:, :, 0], 10)
             pp = np.uint8(cv2.resize(np.clip(img, 0 ,255), (width, height)))
-            #xf = cv2.resize(xf, (2048,2048))
+            xf = cv2.resize(xf, (1024,1024))
+            pp = cv2.resize(pp, (1024, 1024))
             pp, orig_faces, enhanced_faces = enhancer.process(pp, xf, bbox=c, face_enhance=False, possion_blending=True)
             cv2.imwrite('./temp/images/frame{}.png'.format(ip), pp)
             ip+=1
