a7e8c74d35a235283120817e480482d0a78b9b04
Ryukhaan <remi.decelle@protonmail.com>
2024-03-26 16:34:40 +0100

    Change options for prepareing dataset

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -54,13 +54,52 @@ from third_part.ganimation_replicate.model.ganimation import GANimationModel
 from futils import audio
 from futils.ffhq_preprocess import Croper
 from futils.alignment_stit import crop_faces, calc_alignment_coefficients, paste_image
-from futils.inference_utils import Laplacian_Pyramid_Blending_with_mask, face_detect, load_train_model, options, \
+from futils.inference_utils import Laplacian_Pyramid_Blending_with_mask, face_detect, load_train_model, \
     split_coeff, \
     trans_image, transform_semantic, find_crop_norm_ratio, load_face3d_net, exp_aus_dict, save_checkpoint, load_model
 from futils.inference_utils import load_model as fu_load_model
 from futils import hparams, audio
 import warnings
+import argparse
 warnings.filterwarnings("ignore")
+
+
+def options():
+    parser = argparse.ArgumentParser(description='Inference code to lip-sync videos in the wild using Wav2Lip models')
+
+    parser.add_argument('--DNet_path', type=str, default='checkpoints/DNet.pt')
+    parser.add_argument('--face3d_net_path', type=str, default='checkpoints/face3d_pretrain_epoch_20.pth')
+    parser.add_argument('--exp_img', type=str, help='Expression template. neutral, smile or image path', default=None)
+    parser.add_argument('--outfile', type=str, help='Video path to save result')
+    parser.add_argument('--fps', type=float, help='Can be specified only if input is a static image (default: 25)',
+                        default=25., required=False)
+    parser.add_argument('--pads', nargs='+', type=int, default=[0, 20, 0, 0],
+                        help='Padding (top, bottom, left, right). Please adjust to include chin at least')
+    parser.add_argument('--face_det_batch_size', type=int, help='Batch size for face detection', default=4)
+    parser.add_argument('--LNet_batch_size', type=int, help='Batch size for LNet', default=16)
+    parser.add_argument('--img_size', type=int, default=384)
+    parser.add_argument('--crop', nargs='+', type=int, default=[0, -1, 0, -1],
+                        help='Crop video to a smaller region (top, bottom, left, right). Applied after resize_factor and rotate arg. '
+                             'Useful if multiple face present. -1 implies the value will be auto-inferred based on height, width')
+    parser.add_argument('--box', nargs='+', type=int, default=[-1, -1, -1, -1],
+                        help='Specify a constant bounding box for the face. Use only as a last resort if the face is not detected.'
+                             'Also, might work only if the face is not moving around much. Syntax: (top, bottom, left, right).')
+    parser.add_argument('--nosmooth', default=False, action='store_true',
+                        help='Prevent smoothing face detections over a short temporal window')
+    parser.add_argument('--static', default=False, action='store_true')
+    parser.add_argument('--up_face', default='original')
+    parser.add_argument('--one_shot', action='store_true')
+    parser.add_argument('--without_rl1', default=False, action='store_true', help='Do not use the relative l1')
+    parser.add_argument('--tmp_dir', type=str, default='temp', help='Folder to save tmp results')
+    parser.add_argument('--re_preprocess', action='store_true')
+    parser.add_argument('--cropped_image', default=False, action='store_true',
+                        help='Cropped the mouth to paste on the original video')
+    parser.add_argument('--dict_path', default="", help="Path to phones dictionary")
+    parser.add_argument('--json_path', default="", help="Path to JSON MFA result")
+    parser.add_argument('--sync_path', default="checkpoints/lipsync_expert.pth",
+                        help="Path to LipSync Network checkpoints")
+    args = parser.parse_args()
+    return args
 
 args = options()
 hparams = hparams.hparams
