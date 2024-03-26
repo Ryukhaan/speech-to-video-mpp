af1b0b6f5b3b53ee8ecc338796b140d1782a997a
Ryukhaan <remi.decelle@protonmail.com>
2024-03-26 13:50:01 +0100

    Change loading lora model

--- inference.py
+++ inference.py
@@ -137,7 +137,6 @@ def main():
                         channel_multiplier=2, bg_upsampler=None)
 
     kp_extractor = KeypointExtractor()
-    print(preprocessor.model)
     for i, (img_batch, mel_batch, frames, coords, img_original, f_frames) in enumerate(tqdm(gen, desc='[Step 6] Lip Synthesis:', total=int(np.ceil(float(delta) / args.LNet_batch_size)))):
         img_batch = torch.FloatTensor(np.transpose(img_batch, (0, 3, 1, 2))).to(device)
         mel_batch = torch.FloatTensor(np.transpose(mel_batch, (0, 3, 1, 2))).to(device)
