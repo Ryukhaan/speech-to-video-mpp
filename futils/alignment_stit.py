683b83d447bb84af6b2d7f439c08ef3ef01dd451
Ryukhaan <remi.decelle@protonmail.com>
2024-04-08 10:33:32 +0200

    Change list variable name

--- futils/alignment_stit.py
+++ futils/alignment_stit.py
@@ -160,7 +160,7 @@ def crop_faces(IMAGE_SIZE, files, scale, center_sigma=0.0, xy_sigma=0.0, use_fa=
         # detector = dlib.get_frontal_face_detector()
 
     cs, xs, ys = [], [], []
-    for lm, pil in tqdm(files):
+    for lm, pil in files:
         c, x, y = compute_transform(lm, predictor, detector=detector,
                                     scale=scale, fa=fa)
         cs.append(c)
@@ -188,7 +188,7 @@ def crop_faces(IMAGE_SIZE, files, scale, center_sigma=0.0, xy_sigma=0.0, use_fa=
 def crop_faces_by_quads(IMAGE_SIZE, files, quads):
     orig_images = []
     crops = []
-    for quad, (_, path) in tqdm(zip(quads, files), total=len(quads)):
+    for quad, (_, path) in zip(quads, files):
         crop = crop_image(path, IMAGE_SIZE, quad.copy())
         orig_image = path # Image.open(path)
         orig_images.append(orig_image)
