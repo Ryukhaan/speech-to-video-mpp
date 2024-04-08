a950c4d03788c8fe228710059442cabd3c3da50b
Ryukhaan <remi.decelle@protonmail.com>
2024-04-08 10:32:20 +0200

    Change list variable name

--- from_scratch.py
+++ from_scratch.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
             ff[int(oy1):int(oy2), int(ox1):int(ox2)] = cv2.resize(np.array(imc_pil.convert('RGB')), (ox2 - ox1, oy2 - oy1))
             oface, coords = face_det
             y1, y2, x1, x2 = coords
-            cv2.imwrite("./results/{}.png".format(i), ff[y1:y2, x1:x2])
+            cv2.imwrite("./results/{}.png".format(i), ff) #ff[y1:y2, x1:x2])
             i += 1
             #refs.append(ff[y1: y2, x1:x2])
             out.write(ff[y1:y2, x1:x2])
