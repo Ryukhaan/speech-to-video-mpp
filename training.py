f5f83d2bb56f44efae0a1922cd2dbb13e67616d2
Ryukhaan <remi.decelle@protonmail.com>
2024-04-09 10:00:06 +0200

    Change in save disc model

--- training.py
+++ training.py
@@ -841,8 +841,8 @@ if __name__ == "__main__":
         r=16,
         lora_alpha=16,
         target_modules=["mlp_gamma", "mlp_beta",
-                        "convl2l", "convl2g", "convg2l", "convg2g.conv1.0",
-                        "conv2"],
+                        "convl2l", "convl2g", "convg2l",
+                        "convg2g.conv1.0", "convg2g.conv2"],
         lora_dropout=0.1,
         bias="none",
     )
