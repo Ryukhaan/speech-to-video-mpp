b84c23c30bdb9ac19ccae7f8521a388087a4a117
Ryukhaan <remi.decelle@protonmail.com>
2024-04-10 11:34:13 +0200

    Change LR

--- models/losses.py
+++ models/losses.py
@@ -145,5 +145,5 @@ class LoraLoss(torch.nn.Module):
         tv_val = self.tv_loss(y_pred_up).to(self.device)
         return self.lambda_1 * l1_val \
             + self.lambda_p * lp_val \
-            + self.lambda_ssim * lssim_val \
-            + self.lambda_tv * tv_val
+            #+ self.lambda_ssim * lssim_val \
+            #+ self.lambda_tv * tv_val
