3b8ed9695b36f1efc8013bb32cbaf0601ec29658
Ryukhaan <remi.decelle@protonmail.com>
2024-04-16 14:02:59 +0200

    encodec model correction

--- preprocess.py
+++ preprocess.py
@@ -137,7 +137,7 @@ def encode_audio(vfile, model, gpu_id):
             encoded_frames = audios_model[gpu_id].encode(chunk)
         codes = torch.cat([encoded[0] for encoded in encoded_frames], dim=-1)  # [B, n_q, T]
         codes_chunks.append(np.array(codes))
-
+    print(np.array(codes_chunks).shape)
     np.save(path.join(fulldir, 'audio_features.npy'), np.array(codes_chunks))
 
 def mp_handler(job):
