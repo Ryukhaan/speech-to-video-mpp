e113c5d86c57fc012826036578ea386d7ebe11ac
Ryukhaan <remi.decelle@protonmail.com>
2024-04-18 11:15:35 +0200

    Debug text encoder

--- preprocess.py
+++ preprocess.py
@@ -140,7 +140,7 @@ def encode_audio(vfile, args, gpu_id):
     #for batch in audio_chunks:
     chunk = convert_audio(wav, sr, audios_model[gpu_id].sample_rate, audios_model[gpu_id].channels)
     chunk = chunk.unsqueeze(0)
-    chunk = torch.nn.functional.pad(chunk, (0, int(args.chunk_length_s * sr), 0, 0, 0, 0), "constant", 0)
+    chunk = torch.nn.functional.pad(chunk, (0, int(2 * args.chunk_length_s * sr), 0, 0, 0, 0), "constant", 0)
 
     # Extract discrete codes from EnCodec
     with torch.no_grad():
