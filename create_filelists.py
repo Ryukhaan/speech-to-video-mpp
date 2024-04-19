fa38174d5ec147451c305ca5a5a8e60b08eb13f6
Ryukhaan <remi.decelle@protonmail.com>
2024-04-19 09:34:37 +0200

    Include new training file

---
+++ create_filelists.py
@@ -0,0 +1,16 @@
+import os
+import glob
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Code to train the Wav2Lip model WITH the visual quality discriminator')
+
+parser.add_argument("--data_root", help="Root folder of the preprocessed LRS2 dataset", required=True, type=str)
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    directories = glob.glob(os.path.join(args.data_root, '*/*'))
+    with open('train-lrs2.txt', 'w') as file:
+        for directory in directories:
+            file.write(directory)
