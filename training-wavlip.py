9adce1e739445a06aff6e13b51d88d77b70cbda4
Ryukhaan <remi.decelle@protonmail.com>
2024-04-19 09:42:09 +0200

    Include new training file

--- training-wavlip.py
+++ training-wavlip.py
@@ -122,7 +122,9 @@ class Dataset(object):
 
         audiopath = join(vidname, "audio_features.npy")
         audio_features = np.load(audiopath, allow_pickle=True)
+        print(audio_features.shape)
         fa = audio_features[start_frame_num: start_frame_num+syncnet_T, ...]
+        print(fa.shape)
         return torch.cat([fa[:,:,i] for i in range(fa.shape[2])], dim=1)
 
 
